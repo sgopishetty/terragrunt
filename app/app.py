5c6023551e6ab1662e138778bc874217e6522f35
sgopishetty <anuragsai05@gmail.com>
2024-07-28 19:09:00 +0530

    cicd 11

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! 1.0'
+    return 'Hello, World! 1.1'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
