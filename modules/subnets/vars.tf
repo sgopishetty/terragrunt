bb71f03c0e349657855341951fd2efe06ec7d552
sgopishetty <anuragsai05@gmail.com>
2024-08-21 08:17:28 +0530

    uat 28

--- modules/subnets/vars.tf
+++ modules/subnets/vars.tf
@@ -43,8 +43,8 @@ variable "internet_gateway_id" {
   type = string
 }
 
-#variable "availability_zones" {
-#  description = "List of availability zones to deploy resources into."
-#  type        = list(string)
-#  default     = data.aws_availability_zones.available.names
-#}
+variable "availability_zones" {
+  description = "List of availability zones to deploy resources into."
+  type        = list(string)
+  default     = ["us-east-1a","us-east-1b","us-east-1c","us-east-1d"]
+}
