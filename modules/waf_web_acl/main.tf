fd65bf726207634eb6322f90923bd57de680c2c7
sgopishetty <anuragsai05@gmail.com>
2024-10-02 18:01:58 +0530

    waf new 9

--- modules/waf_web_acl/main.tf
+++ modules/waf_web_acl/main.tf
@@ -16,24 +16,103 @@ resource "aws_wafv2_web_acl" "this" {
     for_each = var.rules
     content {
       name     = rule.value.name
-      priority = index(var.rules, rule.value)
-      override_action {
-        none {}  # Set to 'block {}' to block instead of count
-      }
+      priority = rule.value.priority
+
       statement {
-        managed_rule_group_statement {
-          vendor_name = "AWS"
-          name        = rule.value.rule_id
+        # Handle managed rule group statements
+        dynamic "managed_rule_group_statement" {
+          for_each = lookup(rule.value, "rule_id", null) != null ? [rule.value] : []
+          content {
+            vendor_name = "AWS"
+            name        = managed_rule_group_statement.value.rule_id
+
+            # Logic to exclude specific sub-rules
+            dynamic "excluded_rule" {
+              for_each = lookup(managed_rule_group_statement.value, "excluded_rules", [])
+              content {
+                name = excluded_rule.value  # Name of the sub-rule to exclude
+              }
+            }
+          }
+        }
+
+        # Handle custom regex match statements
+        dynamic "regex_match_statement" {
+          for_each = lookup(rule.value, "regex_match_statement", [])
+          content {
+            regex_string = regex_match_statement.value.regex_string
+
+            field_to_match {
+              uri_path {}
+            }
+
+            # Dynamically handle the required text_transformation block
+            dynamic "text_transformation" {
+              for_each = regex_match_statement.value.text_transformations
+              content {
+                priority = text_transformation.value.priority
+                type     = text_transformation.value.type
+              }
+            }
+          }
+        }
+
+         # Handle custom AndStatement
+        dynamic "and_statement" {
+          for_each = lookup(rule.value, "and_statement", [])
+          content {
+            statement {
+              label_match_statement {
+                scope = and_statement.value.label_match_statement_scope
+                key   = and_statement.value.label_match_statement_key
+              }
+
+              not_statement {
+                statement {
+                  regex_match_statement {
+                    regex_string = and_statement.value.not_statement.regex_string
+
+                    field_to_match {
+                      uri_path {}
+                    }
+
+                    # Dynamically handle the required text_transformation block
+                    dynamic "text_transformation" {
+                      for_each = and_statement.value.not_statement.text_transformations
+                      content {
+                        priority = text_transformation.value.priority
+                        type     = text_transformation.value.type
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
         }
       }
+
+      # Override action block
+      dynamic "override_action" {
+        for_each = rule.value.override_action != null ? [rule.value.override_action] : []
+        content {
+          count {}  # Override action to count
+        }
+      }
+
+      action {
+        block {}  # Default action is block
+      }
+
       visibility_config {
         cloudwatch_metrics_enabled = true
-        metric_name                = rule.value.name
+        metric_name                = rule.value.metric_name
         sampled_requests_enabled   = true
       }
     }
   }
 }
+
 
 # Association between WAF and ALB
 resource "aws_wafv2_web_acl_association" "this" {
--- modules/waf_web_acl/vars.tf
+++ modules/waf_web_acl/vars.tf
@@ -11,8 +11,25 @@ variable "scope" {
 variable "rules" {
   description = "List of rules to apply to the Web ACL"
   type = list(object({
-    name    = string
-    rule_id = string
+    name                = string
+    rule_id             = optional(string)  # For managed rules, rule_id will be provided
+    priority            = number            # Priority for the rule
+    excluded_rules      = optional(list(string))  # List of sub-rules to exclude from managed rule group
+    override_action     = optional(string)  # Override action (e.g., "count")
+    regex_match_statement = optional(list(object({  # For custom regex match statements
+      regex_string                = string
+      text_transformation_priority = number
+      text_transformation_type    = string
+    })))
+    and_statement = optional(list(object({  # For complex rules with label and not-statement
+      label_match_statement_scope = string
+      label_match_statement_key   = string
+      not_statement = object({
+        regex_string                = string
+        text_transformation_priority = number
+        text_transformation_type    = string
+      })
+    })))
   }))
 }
 
