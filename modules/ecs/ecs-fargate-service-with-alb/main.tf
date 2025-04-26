7569fd40b9f2815dd6414e86e6c35e76a7c21696
sgopishetty <anuragsai05@gmail.com>
2025-04-26 14:48:38 +0530

    bg22

--- modules/ecs/ecs-fargate-service-with-alb/main.tf
+++ modules/ecs/ecs-fargate-service-with-alb/main.tf
@@ -547,7 +547,6 @@ resource "aws_s3_object" "upload_appspec" {
   bucket = var.app_spec_bucket
   key    = "dev-app-spec/appspec.yaml" # you can customize the key/path
   source = local_file.appspec.filename
-  etag   = filemd5(local_file.appspec.filename)
 
   content_type = "text/yaml"
 
