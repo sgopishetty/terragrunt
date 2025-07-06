e90d087fc7f1585dd4045a5c1edd2bce2eb9a884
sgopishetty <33719396+sgopishetty@users.noreply.github.com>
2025-07-06 17:19:02 +0000

    bg91

--- modules/ecs/ecs-service/main.tf
+++ modules/ecs/ecs-service/main.tf
@@ -185,17 +185,11 @@ resource "aws_ecs_service" "service_with_auto_scaling" {
   #  }
   #}
 
-  # Handle load_balancer for standard or CodeDeploy
-  dynamic "load_balancer" {
-  for_each = var.deployment_controller == "CODE_DEPLOY" ? 
-    toset([local.blue_target_group]) : toset(keys(aws_lb_target_group.ecs_service))
-
-  content {
-    target_group_arn = aws_lb_target_group.ecs_service[load_balancer.value].arn
-    container_name   = var.elb_target_groups[load_balancer.value].container_name
-    container_port   = var.elb_target_groups[load_balancer.value].container_port
+  load_balancer {
+    target_group_arn = aws_lb_target_group.ecs_service[local.blue_target_group].arn
+    container_name   = var.elb_target_groups[local.blue_target_group].container_name
+    container_port   = var.elb_target_groups[local.blue_target_group].container_port
   }
-}
 
   
 
